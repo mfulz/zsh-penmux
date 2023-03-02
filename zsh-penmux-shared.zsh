7a6213971703dde809d6074e728ae5b42915febe
Matthias Fulz <mfulz@olznet.de>
2023-03-02 01:50:27 +0100

    External session handling. Split view.

--- zsh-penmux-shared.zsh
+++ zsh-penmux-shared.zsh
@@ -11,6 +11,7 @@ _if_tmux() {
 }
 
 _if_penmux_session() {
+    _if_tmux || return 1
     tmux show-environment PENMUX_SESSION >/dev/null 2>&1 || return 1
 }
 
@@ -24,6 +25,29 @@ _get_task_name() {
     tmux display-message -p "#{window_name}"
 }
 
+_get_task_id() {
+    tmux display-message -p "#{window_id}"
+}
+
+_get_session_name_by_pid() {
+    local PANE_ID="${1}"
+
+    echo "$(tmux list-panes -aF "#S" -f "#{==:#D,"${PANE_ID}"}")"
+}
+
+_get_task_id_by_name() {
+    local SESSION_NAME="${1}"
+    local TASK_NAME="${2}"
+
+    echo "$(tmux list-windows -aF "#{window_id}" -f "#{==:#S#W,${SESSION_NAME}${TASK_NAME}}")"
+}
+
+_get_task_name_by_id() {
+    local TASK_ID="${1}"
+
+    echo "$(tmux list-windows -aF "#W" -f "#{==:#{window_id},${TASK_ID}}")"
+}
+
 _get_session_name() {
     # dont use directly
     tmux display-message -p "#{session_name}"
@@ -35,51 +59,56 @@ _get_existing_session() {
 }
 
 _if_tmux_env_var() {
-    local ENV_NAME="${1}"
-    tmux show-environment "${ENV_NAME}" >/dev/null 2>&1 || return 1
+    local SESSION_NAME="${1}"
+    local ENV_NAME="${2}"
+    tmux show-environment -t "${SESSION_NAME}" "${ENV_NAME}" >/dev/null 2>&1 || return 1
 }
 
 _get_tmux_env_val() {
-    local ENV_NAME="${1}"
+    local SESSION_NAME="${1}"
+    local ENV_NAME="${2}"
     local ENV_VAR=""
 
-    tmux show-environment "${ENV_NAME}" >/dev/null || return 1
-    ENV_VAR="$(tmux show-environment ${ENV_NAME})"
+    tmux show-environment -t "${SESSION_NAME}" "${ENV_NAME}" >/dev/null || return 1
+    ENV_VAR="$(tmux show-environment -t "${SESSION_NAME}" "${ENV_NAME}")"
 
     ENV_VAL=("${(@s/=/)ENV_VAR}")
     echo ${ENV_VAL[2]}
 }
 
 _get_tmux_env_list() {
-    local ENV_VAR="${1}"
+    local SESSION_NAME="${1}"
+    local ENV_VAR="${2}"
     local ENV_VAL=""
 
-    _if_tmux_env_var "${ENV_VAR}" || tmux set-environment "${ENV_VAR}"
-    ENV_VAL=$(_get_tmux_env_val "${ENV_VAR}")
+    _if_tmux_env_var "${SESSION_NAME}" "${ENV_VAR}" || tmux set-environment -t "${SESSION_NAME}" "${ENV_VAR}"
+    ENV_VAL=$(_get_tmux_env_val "${SESSION_NAME}" "${ENV_VAR}")
 
     LIST_VAL=("${(@s/#/)ENV_VAL}")
     echo ${LIST_VAL}
 }
 
 _add_tmux_env_list() {
-    local ENV_VAR="${1}"
-    local ENV_ENTRY="${2}"
+    local SESSION_NAME="${1}"
+    local ENV_VAR="${2}"
+    local ENV_ENTRY="${3}"
     local ENV_VAL=""
 
-    _in_tmux_env_list "${ENV_VAR}" "${ENV_ENTRY}" && return 0
+    _in_tmux_env_list "${SESSION_NAME}" "${ENV_VAR}" "${ENV_ENTRY}" && return 0
 
-    ENV_VAL="$(_get_tmux_env_val "${ENV_VAR}")#${ENV_ENTRY}"
-    tmux set-environment "${ENV_VAR}" "${ENV_VAL}"
+    ENV_VAL="$(_get_tmux_env_val "${SESSION_NAME}" "${ENV_VAR}")#${ENV_ENTRY}"
+    tmux set-environment -t "${SESSION_NAME}" "${ENV_VAR}" "${ENV_VAL}"
 }
 
 _del_tmux_env_list() {
-    local ENV_VAR="${1}"
-    local ENV_ENTRY="${2}"
+    local SESSION_NAME="${1}"
+    local ENV_VAR="${2}"
+    local ENV_ENTRY="${3}"
     local ENV_VAL=""
 
-    _in_tmux_env_list "${ENV_VAR}" "${ENV_ENTRY}" || return 0
+    _in_tmux_env_list "${SESSION_NAME}" "${ENV_VAR}" "${ENV_ENTRY}" || return 0
 
-    for e in $(_get_tmux_env_list "${ENV_VAR}"); do
+    for e in $(_get_tmux_env_list "${SESSION_NAME}" "${ENV_VAR}"); do
         if [[ "${e}" != "${ENV_ENTRY}" ]]; then
             if [[ "${ENV_VAL}" == "" ]]; then
                 ENV_VAL="${e}"
@@ -89,15 +118,16 @@ _del_tmux_env_list() {
         fi
     done
 
-    tmux set-environment "${ENV_VAR}" "${ENV_VAL}"
+    tmux set-environment -t "${SESSION_NAME}" "${ENV_VAR}" "${ENV_VAL}"
 }
 
 _in_tmux_env_list() {
-    local ENV_VAR="${1}"
-    local ENV_ENTRY="${2}"
+    local SESSION_NAME="${1}"
+    local ENV_VAR="${2}"
+    local ENV_ENTRY="${3}"
 
     local ENTRY_EXISTS=1
-    for e in $(_get_tmux_env_list "${ENV_VAR}"); do
+    for e in $(_get_tmux_env_list "${SESSION_NAME}" "${ENV_VAR}"); do
         if [[ "${e}" == "${ENV_ENTRY}" ]]; then
             ENTRY_EXISTS=0
             break
@@ -105,4 +135,38 @@ _in_tmux_env_list() {
     done
 
     return ${ENTRY_EXISTS}
- }
+}
+
+_if_action_duplicate_by_id() {
+    local PANE_ID="${1}"
+    local ACTION_NAME="${2}"
+
+    local _swt="$(tmux list-panes -aF "#S #W" -f "#{==:#{pane_id},${PANE_ID}}")"
+
+    _if_action_duplicate ${=_swt} "${ACTION_NAME}"
+}
+
+_if_action_duplicate() {
+    local SESSION_NAME="${1}"
+    local TASK_NAME="${2}"
+    local ACTION_NAME="${3}"
+
+    local _pane="$(tmux list-panes -aF "#T" -f "#{==:#S#W#T,${SESSION_NAME}${TASK_NAME}${ACTION_NAME}}")"
+
+    if [[ "${_pane}" == "" ]]; then
+        return 0
+    fi
+    return 1
+}
+
+_if_task_name_unique() {
+    local SESSION_NAME="${1}"
+    local TASK_NAME="${2}"
+
+    local _count=$(tmux list-windows -af "#{==:#S#W,${SESSION_NAME}${TASK_NAME}}" | wc -l)
+
+    if [[ "${_count}" -eq 1 ]]; then
+        return 0
+    fi
+    return 1
+}
--- zsh-penmux.zsh
+++ zsh-penmux.zsh
@@ -31,7 +31,6 @@ penmux_create_session() {
     local ACTION_NAME="SCAN"
     local WORK_DIR="$(pwd)"
     local LOG=1
-    local SESSION_EXISTS="$(_get_existing_session "${SESSION_NAME}")"
 
     local OPTIND o
     while getopts "s:t:a:lnh" o; do
@@ -59,9 +58,10 @@ penmux_create_session() {
                 ;;
         esac
     done
-
+    local SESSION_EXISTS="$(_get_existing_session "${SESSION_NAME}")"
+    
     if [[ "${SESSION_EXISTS}" != "" ]]; then
-        >&2 echo "Session already exists"; return 1 }
+        { >&2 echo "Session already exists"; return 1 }
     else
         tmux new-session -dc "${WORK_DIR}" -s "${SESSION_NAME}" \; \
             set-option -q "@la-work-dir" "${WORK_DIR}" \; \
@@ -81,7 +81,6 @@ penmux_create_session() {
 
 penmux_attach_session() {
     local SESSION_NAME="$(date +"%Y%m%dT%H%M")"
-    local SESSION_EXISTS="$(_get_existing_session "${SESSION_NAME}")"
 
     local OPTIND o
     while getopts "s:t:a:lnh" o; do
@@ -97,6 +96,7 @@ penmux_attach_session() {
                 ;;
         esac
     done
+    local SESSION_EXISTS="$(_get_existing_session "${SESSION_NAME}")"
 
     if [[ "${SESSION_EXISTS}" != "" ]]; then
         _if_tmux && { >&2 echo "Tmux detected. Please exit first."; return 1 }
@@ -137,67 +137,251 @@ penmux_set_task() {
 }
 
 penmux_new_task() {
-    _if_tmux || { >&2 echo "No tmux session"; return 1 }
-    _if_penmux_session || { >&2 echo "No penmux session"; return 1 }
-    local TASK_NAME="${1}"
+    local SESSION_NAME=""
+    local TASK_NAME=""
+    local ACTION_NAME=""
+    local LOG=1
 
-    tmux new-window -n "${TASK_NAME}"
+    local OPTIND o
+    while getopts "s:t:a:n" o; do
+        case "${o}" in
+            s)
+                SESSION_NAME="${OPTARG}"
+                ;;
+            t)
+                TASK_NAME="${OPTARG}"
+                ;;
+            a)
+                ACTION_NAME="${OPTARG}"
+                ;;
+            n)
+                LOG=0
+                ;;
+            *)
+                echo "TODO: Help" && return 1
+                ;;
+        esac
+    done
+
+    if [[ "${TASK_NAME}" == "" ]]; then
+        { >&2 echo "Task name required"; return 1 }
+    fi
+
+    if [[ "${ACTION_NAME}" == "" ]]; then
+        { >&2 echo "Action name required"; return 1 }
+    fi
+
+    if [[ "${SESSION_NAME}" == "" ]]; then
+        _if_penmux_session || { >&2 echo "No penmux session"; return 1 }
+        SESSION_NAME="$(_get_session_name)"
+    fi
+
+    _if_action_duplicate "${SESSION_NAME}" "${TASK_NAME}" "${ACTION_NAME}" || { >&2 echo "Action already exiting"; return 1 }
+    local _pane="$(tmux new-window -t "${SESSION_NAME}" -n "${TASK_NAME}" -F "#D" -P)"
+
+    tmux select-pane -t "${_pane}" -T "${ACTION_NAME}"
+
+    if [[ "${LOG}" -ne 0 ]]; then
+        _add_tmux_env_list "${SESSION_NAME}" PENMUX_LOG_ACTIONS "${_pane}"
+    fi
 }
 
-penmux_set_action() {
-    _if_tmux || { >&2 echo "No tmux session"; return 1 }
-    _if_penmux_session || { >&2 echo "No penmux session"; return 1 }
+penmux_rename_action() {
     local ACTION_NAME="${1}"
+    local SESSION_NAME="$(_get_session_name)"
+    local PANE_ID="$(_get_action_id)"
 
-    tmux select-pane -T "${ACTION_NAME}"
+    if [[ "${2}" != "" ]]; then
+        PANE_ID="${2}"
+        SESSION_NAME="$(_get_session_name_by_pid "${PANE_ID}")"
+    fi
+
+    if [[ "${SESSION_NAME}" == "" ]]; then
+        { >&2 echo "Invalid pand id. Session not found"; return 1 }
+    fi
+
+    _if_action_duplicate_by_id "${PANE_ID}" "${ACTION_NAME}" || { >&2 echo "Action already exiting"; return 1 }
+    tmux select-pane -t "${PANE_ID}" -T "${ACTION_NAME}"
 }
 
 penmux_new_action() {
-    local ACTION_NAME="${1}"
-    local TASK_NAME="$(_get_task_name)"
+    local SESSION_NAME=""
+    local TASK_NAME=""
+    local TASK_ID=""
+    local ACTION_NAME=""
+    local LOG=1
+    local TMUX_FLAGS=""
 
-    penmux_new_task "${TASK_NAME}"
-    penmux_set_action "${ACTION_NAME}"
+    local OPTIND o
+    while getopts "s:t:i:a:nbdhv" o; do
+        case "${o}" in
+            s)
+                SESSION_NAME="${OPTARG}"
+                ;;
+            t)
+                TASK_NAME="${OPTARG}"
+                ;;
+            i)
+                TASK_ID="${OPTARG}"
+                ;;
+            a)
+                ACTION_NAME="${OPTARG}"
+                ;;
+            n)
+                LOG=0
+                ;;
+            b)
+                TMUX_FLAGS="${TMUX_FLAGS} -b"
+                ;;
+            d)
+                TMUX_FLAGS="${TMUX_FLAGS} -d"
+                ;;
+            h)
+                TMUX_FLAGS="${TMUX_FLAGS} -h"
+                ;;
+            v)
+                TMUX_FLAGS="${TMUX_FLAGS} -v"
+                ;;
+            *)
+                echo "TODO: Help" && return 1
+                ;;
+        esac
+    done
+
+    if [[ "${ACTION_NAME}" == "" ]]; then
+        { >&2 echo "Action name required"; return 1 }
+    fi
+
+    if [[ "${SESSION_NAME}" == "" ]]; then
+        _if_penmux_session || { >&2 echo "No penmux session"; return 1 }
+        SESSION_NAME="$(_get_session_name)"
+    fi
+
+    if [[ "${TASK_NAME}" != "" && "${TASK_ID}" != "" ]]; then
+        { >&2 echo "Specify either TASK_NAME or TASK_ID not both"; return 1 }
+    elif [[ "${TASK_NAME}" != "" ]]; then
+        _if_task_name_unique "${SESSION_NAME}" "${TASK_NAME}" || { >&2 echo "Task '${TASK_NAME}' not unique or not existing. Please specify TASK_ID."; return 1 }
+        TASK_ID="$(_get_task_id_by_name "${SESSION_NAME}" "${TASK_NAME}")"
+    elif [[ "${TASK_ID}" != "" ]]; then
+        TASK_NAME="$(_get_task_name_by_id "${TASK_ID}")"
+        _if_task_name_unique "${SESSION_NAME}" "${TASK_NAME}" || { >&2 echo "Task ID '${TASK_ID}' invalid."; return 1 }
+    else
+        _if_penmux_session || { >&2 echo "No penmux session"; return 1 }
+        TASK_NAME="$(_get_task_name)"
+        TASK_ID="$(_get_task_id)"
+    fi
+
+    _if_action_duplicate "${SESSION_NAME}" "${TASK_NAME}" "${ACTION_NAME}" || { >&2 echo "Action already exiting"; return 1 }
+    _pane="$(tmux split-window ${=TMUX_FLAGS} -t "${TASK_ID}" -P -F "#D")"
+    tmux select-pane -t "${_pane}" -T "${ACTION_NAME}"
+
+    if [[ "${LOG}" -ne 0 ]]; then
+        _add_tmux_env_list "${SESSION_NAME}" PENMUX_LOG_ACTIONS "${_pane}"
+    fi
 }
 
 penmux_toggle_log_action() {
+    local SESSION_NAME="$(_get_session_name)"
     local PANE_ID="$(_get_action_id)"
 
-    _in_tmux_env_list PENMUX_LOG_ACTIONS "${PANE_ID}" && _del_tmux_env_list PENMUX_LOG_ACTIONS "${PANE_ID}" || _add_tmux_env_list PENMUX_LOG_ACTIONS "${PANE_ID}"
+    if [[ "${1}" != "" ]]; then
+        PANE_ID="${1}"
+        SESSION_NAME="$(_get_session_name_by_pid "${PANE_ID}")"
+    fi
+
+    if [[ "${SESSION_NAME}" == "" ]]; then
+        { >&2 echo "Invalid pand id. Session not found"; return 1 }
+    fi
+
+    _in_tmux_env_list "${SESSION_NAME}" PENMUX_LOG_ACTIONS "${PANE_ID}" && _del_tmux_env_list "${SESSION_NAME}" PENMUX_LOG_ACTIONS "${PANE_ID}" || _add_tmux_env_list "${SESSION_NAME}" PENMUX_LOG_ACTIONS "${PANE_ID}"
 }
 
 penmux_add_log_action() {
+    local SESSION_NAME="$(_get_session_name)"
     local PANE_ID="$(_get_action_id)"
 
-    _add_tmux_env_list PENMUX_LOG_ACTIONS "${PANE_ID}"
+    if [[ "${1}" != "" ]]; then
+        PANE_ID="${1}"
+        SESSION_NAME="$(_get_session_name_by_pid "${PANE_ID}")"
+    fi
+
+    if [[ "${SESSION_NAME}" == "" ]]; then
+        { >&2 echo "Invalid pand id. Session not found"; return 1 }
+    fi
+
+    _add_tmux_env_list "${SESSION_NAME}" PENMUX_LOG_ACTIONS "${PANE_ID}"
 }
 
 penmux_del_log_action() {
+    local SESSION_NAME="$(_get_session_name)"
     local PANE_ID="$(_get_action_id)"
 
-    _del_tmux_env_list PENMUX_LOG_ACTIONS "${PANE_ID}"
+    if [[ "${1}" != "" ]]; then
+        PANE_ID="${1}"
+        SESSION_NAME="$(_get_session_name_by_pid "${PANE_ID}")"
+    fi
+
+    if [[ "${SESSION_NAME}" == "" ]]; then
+        { >&2 echo "Invalid pand id. Session not found"; return 1 }
+    fi
+
+    _del_tmux_env_list "${SESSION_NAME}" PENMUX_LOG_ACTIONS "${PANE_ID}"
 }
 
 penmux_get_log_action() {
+    local SESSION_NAME="$(_get_session_name)"
     local PANE_ID="$(_get_action_id)"
+    local STD_OUT=0
 
-    _in_tmux_env_list PENMUX_LOG_ACTIONS "${PANE_ID}" && tmux display-message -d 5000 "penmux logging enabled" || tmux display-message -d 5000 "penmux logging disabled"
+    if [[ "${1}" != "" ]]; then
+        PANE_ID="${1}"
+        SESSION_NAME="$(_get_session_name_by_pid "${PANE_ID}")"
+        STD_OUT=1
+    fi
+
+    if [[ "${SESSION_NAME}" == "" ]]; then
+        { >&2 echo "Invalid pand id. Session not found"; return 1 }
+    fi
+
+    if [[ "${STD_OUT}" -eq 0 ]]; then
+        _in_tmux_env_list "${SESSION_NAME}" PENMUX_LOG_ACTIONS "${PANE_ID}" && tmux display-message -t "${SESSION_NAME}" -d 5000 "penmux logging enabled" || tmux display-message -t "${SESSION_NAME}" -d 5000 "penmux logging disabled"
+    else
+        _in_tmux_env_list "${SESSION_NAME}" PENMUX_LOG_ACTIONS "${PANE_ID}" && echo "penmux logging enabled" || echo "penmux logging disabled"
+    fi
 }
 
 penmux_toggle_log() {
-    for _pane in $(_get_tmux_env_list PENMUX_LOG_ACTIONS); do
+    local SESSION_NAME="$(_get_session_name)"
+
+    if [[ "${1}" != "" ]]; then
+        SESSION_NAME="${1}"
+    fi
+
+    for _pane in $(_get_tmux_env_list "${SESSION_NAME}" PENMUX_LOG_ACTIONS); do
         tmux run -t "${_pane}" "${TMUX_LOGGING_EXTENDED_TOGGLE_LOG} -p "${_pane}""
     done
 }
 
 penmux_start_log() {
-    for _pane in $(_get_tmux_env_list PENMUX_LOG_ACTIONS); do
+    local SESSION_NAME="$(_get_session_name)"
+
+    if [[ "${1}" != "" ]]; then
+        SESSION_NAME="${1}"
+    fi
+
+    for _pane in $(_get_tmux_env_list "${SESSION_NAME}" PENMUX_LOG_ACTIONS); do
         tmux run -t "${_pane}" "${TMUX_LOGGING_EXTENDED_TOGGLE_LOG} -a start -p "${_pane}""
     done
 }
 
 penmux_stop_log() {
-    for _pane in $(_get_tmux_env_list PENMUX_LOG_ACTIONS); do
+    local SESSION_NAME="$(_get_session_name)"
+
+    if [[ "${1}" != "" ]]; then
+        SESSION_NAME="${1}"
+    fi
+
+    for _pane in $(_get_tmux_env_list "${SESSION_NAME}" PENMUX_LOG_ACTIONS); do
         tmux run -t "${_pane}" "${TMUX_LOGGING_EXTENDED_TOGGLE_LOG} -a stop -p "${_pane}""
     done
 }
