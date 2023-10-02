fb153f6ff2f3d0c4e111226844fcdae5d8ffa825
Matthias Fulz <mfulz@olznet.de>
2023-10-03 01:37:19 +0200

    Aliases fixed
    - Fixed aliases to use completions

--- zsh-penmux.zsh
+++ zsh-penmux.zsh
@@ -8,16 +8,14 @@ source "${CURRENT_DIR}/zsh-penmux-cmd.zsh"
 #
 # Aliases
 #
-alias pmCS='penmux_create_session'
-alias pmAS='penmux_attach_session'
-alias pmES='penmux_end_session'
-alias pmNT='penmux_new_task'
-alias pmNA='penmux_new_action'
-alias pmTA='penmux_toggle_log_action'
-alias pmAA='penmux_add_log_action'
-alias pmDA='penmux_del_log_action'
-alias pmGA='penmux_get_log_action'
-alias pmTL='penmux_toggle_log'
-alias pmSL='penmux_start_log'
-alias pmEL='penmux_stop_log'
+alias pmSC='penmux session create'
+alias pmSA='penmux session attach'
+alias pmSD='penmux session destroy'
+alias pmTC='penmux task create'
+alias pmAC='penmux action create'
+alias pmLT='penmux logger toggle'
+alias pmLA='penmux logger add'
+alias pmLR='penmux logger remove'
+alias pmLS='penmux logger start'
+alias pmLE='penmux logger stop'
 
